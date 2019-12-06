8e7460d8e3fd9287d26ead43305c36d8209463de
JuarbeAbraham <55060243+JuarbeAbraham@users.noreply.github.com>
2019-12-05 21:02:07 -0600

    Update codigos.js

--- codigos.js
+++ codigos.js
@@ -1,2 +1,5 @@
 <link href="https://fonts.googleapis.com/css?family=Titillium+Web:600&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="https://cdn.plyr.io/3.5.6/plyr.css"/>
+<script>//<![CDATA[
+const players = Array.from(document.querySelectorAll('.js-player')).map(player => new Plyr(player));
+//]]></script>
